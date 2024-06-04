f187aa464848a2793a4185fbfcd203ff5cbec637
Simone Marcato <simone@hevelop.com>
2024-06-04 14:32:56 +0200

    Generated client for service: agent with version: 1.1.0 in language: typescript

---
+++ src/models/index.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AgentAgentEntity';
+export * from './AgentCreateAgentRequest';
+export * from './AgentListAgentsRequest';
+export * from './AgentListResponse';
+export * from './AgentSortOrder';
+export * from './AgentUpdateAgentRequest';
+export * from './AgentUpdatePayload';
+export * from './ListRequestFilters';
+export * from './ListRequestSort';
+export * from './ProtobufAny';
+export * from './RpcStatus';
+export * from './SortSortField';
